51bce479ad2877bf8f8c9bc6efc8abef7e2d9eea
Jrwolf99 <jonathanwolf456@gmail.com>
2024-01-21 17:36:00 -0600

    bug fix

--- app/controllers/api/v1/spends_controller.rb
+++ app/controllers/api/v1/spends_controller.rb
@@ -6,6 +6,11 @@ module Api
       def update
         my_spend = Spend.find(spend_params[:id])
 
+        if spend_category_params[:identifier].present?
+          spend_category = SpendCategory.find_by(identifier: spend_category_params[:identifier])
+          my_spend.update(spend_category_id: spend_category.id)
+        end
+
         if my_spend.update(spend_params)
           my_spend.update(locked_from_importer_at: Time.now) if spend_params[:date_of_spend].present?
           render json: my_spend
@@ -17,7 +22,11 @@ module Api
       private
 
       def spend_params
-        params.require(:spend).permit(:id, :spend_category_id, :date_of_spend, :amount, :notes)
+        params.require(:spend).permit(:id, :date_of_spend, :amount, :notes)
+      end
+
+      def spend_category_params
+        params.require(:spend_category).permit(:identifier)
       end
     end
   end
